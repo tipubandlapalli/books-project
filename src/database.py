3b04fc20b3b4cb94da335976a8844b4e92330abb
Tipu <bstippusultan@gmail.com>
2026-05-23 18:21:03 +0530

    database.py

---
+++ src/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE_CONNECT_URL = "sqlite:///./books.db"
+
+engine = create_engine(URL=DATABASE_CONNECT_URL, connect_args={'check_same_thread':False})
+
+SessionLocal = sessionmaker(autoflush=False, expire_on_commit=False,bind=engine)
+
+Base = declarative_base()
