e651256143c32ebd30b796c51d9229a3fe833b46
Tipu <bstippusultan@gmail.com>
2026-05-24 14:41:36 +0530

    CRUD

--- src/main.py
+++ src/main.py
@@ -1,33 +1,56 @@
-from fastapi import FastAPI, HTTPException
-from book import Book
+from fastapi import FastAPI, HTTPException, Depends, Path, status
+from typing import Annotated
+from sqlalchemy.orm import Session
 
-import models
-from database import engine
+from models import Books, Base, BookRequest
+from database import engine, SessionLocal
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
-BOOKS = {
-    1: Book(id=1, title="A", description="description", price=120),
-    2: Book(id=2, title="B", description="description", price=104),
-    3: Book(id=3, title="C", description="description", price=104),
-    4: Book(id=4, title="D", description="description", price=1025),
-}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
 
 @app.get("/v1/books")
-async def all_books():
-    return BOOKS
+async def all_books(db: db_dependency):
+    return db.query(Books).all()
 
+@app.get("/v1/books/{book_id}")
+async def book_by_id(db: Annotated[Session, Depends(get_db)], book_id:int = Path(gt=0)):
+    res =  db.query(Books).filter(Books.id == book_id).first()
+    if res is None: raise HTTPException(status_code=404, detail="Book not found")
+    return res
 
-@app.get("/v1/books/{id}")
-async def get_book_by_id(id: int):
-    if id in BOOKS:
-        return BOOKS[id]
-    raise HTTPException(status_code=404, detail="book not found")
+@app.post("/v1/books", status_code=status.HTTP_201_CREATED)
+async def create_new_book(db: db_dependency, book_request: BookRequest):
+    book_model = Books(**book_request.model_dump())
+    db.add(book_model)
+    db.commit()
+    db.refresh(book_model)
+    return book_model
 
-@app.post("/v1/books")
-async def create_new_book(book: Book):
-    if book.id in BOOKS:
-        raise HTTPException(status_code=409, detail="id already exists")
-    BOOKS[book.id] = book
+@app.put("/v1/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def modify_book(book_request: BookRequest, db: db_dependency, book_id:int = Path(gt=0)):
+    book_model = db.query(Books).filter(Books.id == book_id).first() 
+    if book_model is None: raise HTTPException(status_code=404, detail="Book not found")
+    book_model.description = book_request.description
+    book_model.price = book_request.price
+    book_model.title = book_request.title
+    db.add(book_model)
+    db.commit()
+    db.refresh(book_model)
+
+@app.delete("/v1/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def del_book(db: db_dependency, book_id: int = Path(gt=0)):
+    book_model = db.query(Books).filter(Books.id == book_id).first() 
+    if book_model is None: raise HTTPException(status_code=404, detail="Book not found")
+
+    db.query(Books).filter(Books.id == book_id).delete()
+    db.commit()
--- src/models.py
+++ src/models.py
@@ -1,12 +1,18 @@
 from database import Base
 from sqlalchemy import Integer, Column, String, Float
+from pydantic import BaseModel, Field
 
 class Books(Base):
-    __tablename__ = "Books"
+    __tablename__ = 'books'
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    title = Column(String, nullable=False)
     description = Column(String)
     price = Column(Float, default=0)
 
+
+class BookRequest(BaseModel):
+    title:str = Field(min_length=1, max_length=10)
+    description:str
+    price: float = Field(ge=0)
     
