9e13fe0350b2102d45b6af1a53e26016283d58e5
Tipu Bandlapalli <144217734+tipubandlapalli@users.noreply.github.com>
2026-05-27 21:50:22 +0530

    ci cd setup (#2)

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from routers import books, auth
+from src.routers import books, auth
 
 app = FastAPI()
 
 app.include_router(books.router)
 app.include_router(auth.router)
 
-# uvicorn main:app --host 127.0.0.1 --port 8000 --reload
+# uvicorn src.main:app --host 127.0.0.1 --port 8000 --reload
